71a2f7b54d58ec3d77ce5b90305b75c0571e0d5e
Athika Priya <athikapriya1997@gmail.com>
2026-03-24 00:47:45 +0600

    Include jobs app URLs in project urls.py

--- hireflow/urls.py
+++ hireflow/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("base.urls")),
     path("accounts/", include("accounts.urls")),
+    path("jobs/", include("jobs.urls")),
 ]
 
 if settings.DEBUG :
