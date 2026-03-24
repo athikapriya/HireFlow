6058a774eaae0e5a55c0402b899e6663b3c8b94c
Athika Priya <athikapriya1997@gmail.com>
2026-03-25 02:44:11 +0600

    Add URLs for employer applications page with accept/reject actions

--- applications/urls.py
+++ applications/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("apply/<int:job_id>/", views.apply_job, name="apply_job"),
-    path("applied-jobs/", views.applied_jobs, name="applied_jobs"),
+    path("candidate/apply/<int:job_id>/", views.apply_job, name="apply_job"),
+    path("candidate/applied-jobs/", views.applied_jobs, name="applied_jobs"),
+
+    path('employer/all_applications/', views.employer_applications, name='employer_applications'),
+    path('employer/application/<int:app_id>/accept/', views.accept_application, name='accept_application'),
+    path('employer/application/<int:app_id>/reject/', views.reject_application, name='reject_application'),
 ]
